ab7524e6aa31902991ef42e622df60613f401f07
Zhuo Chen <zhuo.h.chen@gmail.com>
2018-04-02 14:39:19 -0400

    Add webpack.config. Add babel build tools and react.

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+
+module.exports = {
+  entry: './lib/entry.js',
+  output: {
+    path: path.resolve(__dirname, 'lib'),
+    filename: './bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
