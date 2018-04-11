e464e3408fc6b0c224110f46e91650dc5b3b1655
Zhuo Chen <zhuo.h.chen@gmail.com>
2018-04-10 22:54:17 -0400

    Properly parsing data. Sending data to Globe.

--- lib/app.jsx
+++ lib/app.jsx
@@ -7,7 +7,7 @@ import { worldMap } from './world_map';
 class Who extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { indicator: 0, countries: [], worldMap: {}, title: '' };
+    this.state = { showGlobe: false, indicator: 0, countries: [], globeMap: {}, title: '' };
   }
 
   componentDidMount() {
@@ -19,15 +19,20 @@ class Who extends React.Component {
   }
 
   setData(data) {
+    // debugger
     const countries = Util.formatCountries(data);
-    const worldMap = Util.bindMap(worldMap, countries);
-    const title = worldMap.features.find(el => el.fact).fact.title;
-
-    this.setState({ countries, worldMap, title });
+    const globeMap = Util.bindMap(worldMap, countries);
+    const title = globeMap.features.find(el => el.fact).fact.title;
+debugger
+    this.setState({ countries, globeMap, title, showGlobe: true });
   }
 
   render() {
-    return <Globe dims={ this.props.dims } indicator={ this.state.indicator }/>
+    debugger
+    return (
+      this.state.showGlobe
+      && <Globe dims={ this.props.dims } indicatorValues={ this.state }/>
+    );
   }
 }
 
