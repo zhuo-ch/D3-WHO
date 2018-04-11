a9f814b6ffd8897f3f1c5953dd05272836f04ad0
Zhuo Chen <zhuo.h.chen@gmail.com>
2018-04-10 23:35:20 -0400

    Rendering globe. Rotating globe.

--- lib/globe.jsx
+++ lib/globe.jsx
@@ -8,7 +8,7 @@ import Donut from './donut';
 class Globe extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { globe: '', grat: '', path: '', context: '' };
+    this.state = { globe: '', grat: '', path: '', context: '', initialized: false };
     this.water = { type: 'Sphere' };
     this.colors = { water: '#aacbff', land: '#003fa5', grat: '#89b6ff', country: '#a00' };
     this.colorGrad = ['#3366ff', '#5b84ff', '#6699ff', '#9999ff', '#cc66ff', '#ffa8e2', '#ff66cc', '#ff0066', '#ff0000', '#990000', '#0000ff'];
@@ -19,7 +19,6 @@ class Globe extends React.Component {
 
   componentDidMount() {
     this.setGlobe();
-    this.startGlobe();
   }
 
   setGlobe() {
@@ -27,21 +26,20 @@ class Globe extends React.Component {
     const globe = d3.geoOrthographic().precision(0.1);
     const grat = d3.geoGraticule10();
     const path = d3.geoPath(globe).context(context);
-debugger
-    this.setState({ globe, grat, path, context }, this.scale);
-      // .then(this.scale());
-      // .then(this.setRotation());
-    // this.scale();
+
+    this.setState({ globe, grat, path, context }, this.startGlobe);
   }
 
   scale() {
     const canvas = this.refs.canvas;
     const width = this.props.dims[0], height = this.props.dims[1];
     canvas.width = width, canvas.height = height;
-debugger
-    this.state.globe
+
+    const globe = this.state.globe
       .scale((0.8 * Math.min(width, height))/2)
       .translate([width * 2 / 5, height / 2]);
+
+    this.setState({ canvas, globe, renderDims: [width, height] });
   }
 
   setRotation(timeElapsed) {
@@ -54,18 +52,22 @@ debugger
       xyz[0] += timeDifference * this.coords.rate;
       this.state.globe.rotate(xyz);
       this.coords.x = xyz[0];
-      this.render();
+      this.drawGlobe();
     }
 
     this.prevTime = currentTime;
   }
 
   startGlobe() {
+    this.scale();
+
     if (this.timer) {
       this.timer.restart(this.setRotation);
     } else {
       this.timer = d3.timer(this.setRotation);
     }
+
+    this.setState({ initialized: true });
   }
 
   findColor(country) {
@@ -76,6 +78,18 @@ debugger
 
   getContext() {
     return this.refs.canvas.getContext('2d');
+  }
+
+  drawGlobe() {
+    const { context, path, renderDims } = this.state;
+
+    context.clearRect(0, 0, renderDims[0], renderDims[1]);
+    context.globalAlpha = 0.7;
+    DrawUtil.drawObj(context, path, this.water, this.colors.water);
+    DrawUtil.drawLine(context, path, this.grat, this.colors.grat);
+    DrawUtil.drawMap(context, path, this.props.indicatorValues.globeMap, this.colorGrad);
+
+    this.setState({ context, path });
   }
 
   render() {
