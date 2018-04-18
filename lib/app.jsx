7672280268476e545153a20f8d9bd28551c109c8
Zhuo Chen <zhuo.h.chen@gmail.com>
2018-04-18 18:16:06 -0400

    Add initial navbar. Add initial navbar css.

--- lib/app.jsx
+++ lib/app.jsx
@@ -7,7 +7,8 @@ import { worldMap } from './world_map';
 class Who extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { showGlobe: false, indicator: 0, countries: [], globeMap: {}, title: '' };
+    this.state = { showGlobe: false, indicator: 0, countries: [], globeMap: {}, title: '', selecting: false };
+    this.handleSelectionClick = this.handleSelectionClick.bind(this);
   }
 
   componentDidMount() {
@@ -18,20 +19,84 @@ class Who extends React.Component {
       .then(data => this.setData(data));
   }
 
+  handleSelectionClick(e) {
+    e.preventDefault();
+
+    this.setState({ selecting: this.state.selecting ? false : true });
+  }
+
   setData(data) {
-    // debugger
     const countries = Util.formatCountries(data);
     const globeMap = Util.bindMap(worldMap, countries);
     const title = globeMap.features.find(el => el.fact).fact.title;
-debugger
+
     this.setState({ countries, globeMap, title, showGlobe: true });
   }
 
-  render() {
-    debugger
+  getHeaderItem(text) {
     return (
-      this.state.showGlobe
-      && <Globe dims={ this.props.dims } indicatorValues={ this.state }/>
+      <article>
+        <span className="letter-accent">{ text[0] }</span><span>{ text.slice(1, text.length ) }</span>
+      </article>
+    );
+  }
+
+  getHeader() {
+    const title = ["World", "Health", "Viewer"].map(el => this.getHeaderItem(el));
+
+    return <article className="header">{ title }</article>;
+  }
+
+  getList() {
+    const list = APIUtil.whoList.map(el => el.title);
+    const { selecting } = this.state;
+    const hidden = this.state.selecting ? '' : 'hidden';
+
+    return (
+      <ul className={ `menu-items ${ hidden }`}>
+        { list }
+      </ul>
+    );
+  }
+
+  getSelector() {
+    const selectionList = this.getList();
+
+    return (
+      <article
+        className="menu"
+        onClick={ this.handleSelectionClick }>
+        <span className="menu-title">
+          Select a Data Set
+        </span>
+        { selectionList }
+      </article>
+    );
+  }
+
+  getMenu() {
+    const header = this.getHeader();
+    const selector = this.getSelector();
+
+    return (
+      <div className="nav-bar">
+        { header }
+        { selector }
+      </div>
+    );
+  }
+
+  render() {
+    const menu = this.getMenu();
+
+    return (
+      <div>
+        { menu }
+        {
+          this.state.showGlobe
+          && <Globe dims={ this.props.dims } indicatorValues={ this.state }/>
+        }
+      </div>
     );
   }
 }
